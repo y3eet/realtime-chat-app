be85dbd39075fc35f884ee769b3d760f01595a6c
Yeet <107809331+y3eet@users.noreply.github.com>
2025-01-09 08:25:46 +0800

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+const serverUrl = "http://localhost:5000";
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/api": {
+        target: serverUrl,
+        changeOrigin: true,
+        secure: false,
+      },
+      "/socket.io": {
+        target: serverUrl,
+        changeOrigin: true,
+        ws: true,
+      },
+    },
+    cors: true,
+  },
+});
